7e7fd3fff0ffa2e871d5d57692763504395978b9
Dimas <petrbravlik@gmail.com>
2024-11-14 18:27:37 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('main.html')
+
+@app.route('/1')
+def p1():
+    return render_template('plant 1.html')
+
+@app.route('/2')
+def p2():
+    return render_template('plant 2.html')
+
+@app.route('/3')
+def p3():
+    return render_template('plant 3.html')
+
+@app.route('/4')
+def p4():
+    return render_template('plant 4.html')
+
+
+
+
+if __name__ == '__main__':
+    app.run()
